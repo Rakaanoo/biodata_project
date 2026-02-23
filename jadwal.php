9a080adc8ca8729b08dc3852860ed7dd7d16b57b
Rakaanoo <tarakaabrar@Gmail.com>
2026-02-23 19:20:38 +0700

    Add files via upload

---
+++ jadwal.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jadwal</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    padding: 8px;
+}
+.highlight {
+    background-color: yellow;
+}
+a {
+    text-decoration: none;
+    background: blue;
+    color: white;
+    padding: 8px 15px;
+    border-radius: 5px;
+}
+</style>
+</head>
+<body>
+
+<h2>Jadwal Pelajaran</h2>
+<table>
+<tr>
+<td>SENIN</td>
+<td>MTK</td>
+<td>SJR</td>
+<td>PABP</td>
+<td>BNG</td>
+<td>BIN</td>
+</tr>
+
+<tr>
+<td>SELASA</td>
+<td>IPAS</td>
+<td>PPS</td>
+<td>BJW</td>
+<td>SNM</td>
+<td>-</td>
+</tr>
+
+<tr>
+<td>RABU</td>
+<td>PGD</td>
+<td>PGD</td>
+<td>GIM</td>
+<td>-</td>
+<td>-</td>
+</tr>
+
+<tr>
+<td>KAMIS</td>
+<td>POR</td>
+<td>SJR</td>
+<td>BNG</td>
+<td>MTK</td>
+<td>BIN</td>
+</tr>
+
+<tr>
+<td>JUMAT</td>
+<td>INF</td>
+<td>KKA</td>
+<td>IPAS</td>
+<td>PGD</td>
+<td>-</td>
+</tr>
+</table>
