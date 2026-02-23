4fbaad9b58109253dd36abb2d2b3f4594f1100f3
Rakaanoo <tarakaabrar@Gmail.com>
2026-02-23 18:42:13 +0700

    Add files via upload

---
+++ jadwal.piket.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jadwal</title>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    padding: 8px;
+}
+.highlight {
+    background-color: yellow;
+}
+a {
+    text-decoration: none;
+    background: blue;
+    color: white;
+    padding: 8px 15px;
+    border-radius: 5px;
+}
+</style>
+</head>
+<body>
+<h2>JADWAL PIKET</h2>
+
+<table>
+<tr>
+<td>SENIN</td>
+<td>CINDY</td>
+<td>NATA</td>
+<td>TSABITA</td>
+<td>NAILA</td>
+<td>KARTIKA</td>
+<td>RASYA</td>
+<td>HABIBI</td>
+<td>YUSUF</td>
+</tr>
+
+<tr>
+<td>SELASA</td>
+<td>KEISHA</td>
+<td>SHAFANIRA</td>
+<td>AURORA</td>
+<td>SILA</td>
+<td>SYAUQI</td>
+<td>NAIL</td>
+<td>WAHYU</td>
+</tr>
+
+<tr>
+<td>RABU</td>
+<td>ANIN</td>
+<td>SHIFA</td>
+<td>TANIA</td>
+<td>FASYA</td>
+<td>EKA</td>
+<td>DZAKY</td>
+<td>VINO</td>
+</tr>
+
+<tr>
+<td>KAMIS</td>
+<td>AIDA</td>
+<td>MAHARANI</td>
+<td>TEGAR</td>
+<td>ALYA</td>
+<td>RAVID</td>
+<td>IBNU</td>
+</tr>
+
+<tr>
+<td>JUMAT</td>
+<td>RAKA</td>
+<td>AQILA</td>
+<td>ALAIS</td>
+<td>GENDHIS</td>
+<td>CLAUDIA</td>
+<td>AZIZ</td>
+<td>RAYHAN</td>
+</tr>
+</table>
+
+</body>
